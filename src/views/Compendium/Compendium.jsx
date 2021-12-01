963f2c892ab158aae5057bf4130651caeaa22d63
maddenlockin <maddenlockin@gmail.com>
2021-12-01 13:00:20 -0800

    changed state updating in getFilteredPokemon, commented out sortForm in controls

--- src/views/Compendium/Compendium.jsx
+++ src/views/Compendium/Compendium.jsx
@@ -39,24 +39,24 @@ export default function Compendium() {
     }, []);
 
   //TODO 😖 help!
-  // useEffect(() => {
-  //   async function getFilteredPokemon() {
-  //     if (!selectedType) return;
-  //     setLoading(true);
+  useEffect(() => {
+    async function getFilteredPokemon() {
+      if (!selectedType) return;
+      setLoading(true);
 
-  //     if (selectedType !== 'all') {
-  //       const filteredPokemon = await fetchFilteredPokemon(selectedType);
-  //       setPokemons(filteredPokemon);
-  //     } else {
-  //       const pokemonList = await fetchPokemon();
-  //       this.setState({pokemons: pokemonList});
-  //     }
-  //     setLoading(false);
-  //     setSort('');
-  //   }
+      if (selectedType !== 'all') {
+        const filteredPokemon = await fetchFilteredPokemon(selectedType);
+        setPokemons(filteredPokemon);
+      } else {
+        const pokemonList = await fetchPokemon();
+        setPokemons(pokemonList);
+      }
+      setLoading(false);
+      //setSort('');
+    }
 
-  //   getFilteredPokemon();
-  // }, [selectedType]);
+    getFilteredPokemon();
+  }, [selectedType]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
